3037c6c9261bad3c7ab60668f05d7a50303a0551
Aditya Sonawane <sonawaneaditya096@gmail.com>
2025-02-21 19:19:02 +0530

    job Page

--- Job_Portal/Frontend/src/components/CategoryCarousel.jsx
+++ Job_Portal/Frontend/src/components/CategoryCarousel.jsx
@@ -1,26 +1,31 @@
 import React from 'react'
-import { Carousel, CarouselContent, CarouselItem } from './ui/carousel'
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from './ui/carousel'
+import { Button } from './ui/button'
 
 
-const category=[
-    "Frontend Developer",
-    "Backend Developer",
-    "Data Scienties",
-    "Graphic Designer",
-    "FullStak Developer"
+const category = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Data Scienties",
+  "Graphic Designer",
+  "FullStak Developer"
 ]
 const CategoryCarousel = () => {
   return (
     <div>
-      <Carousel>
+      <Carousel className="w-full max-w-xl mx-auto my-20">
         <CarouselContent>
-            {
-                category.map((cat,index)=>(
-                    <CarouselItem></CarouselItem>
-                ))
-            }
-            
+          {
+            category.map((cat, index) => (
+              <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+                <Button variant="outline" className="rounded-full">{cat}</Button>
+              </CarouselItem>
+            ))
+          }
+
         </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
       </Carousel>
     </div>
   )
