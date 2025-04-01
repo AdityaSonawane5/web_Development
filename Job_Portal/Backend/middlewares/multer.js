c66ba4d3b41d30b2ad1d8dd5fa0c530f92063053
Aditya Sonawane <sonawaneaditya096@gmail.com>
2025-04-01 23:05:21 +0530

    Remove unused files and configurations from frontend and backend

--- Job_Portal/Backend/middlewares/multer.js
+++
@@ -1,9 +0,0 @@
-// import multer from "multer"
-
-// const stroage=multer.memoryStorage();
-// export const SingleUpload=multer({stroage}).single("file")
-
-import multer from "multer";
-
-const storage = multer.memoryStorage(); // Fix the typo here
-export const SingleUpload = multer({ storage }).single("file"); // Use "storage"
--- Job_Portal/Frontend/src/utils/constant.js
+++
@@ -1,4 +0,0 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/User";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/Job";
-
-export const APPLICATION_JOB_END_POINT="http://localhost:8000/api/v1/Application";
